176695f7af02a86bd485742638f2317643e704c9
Jerry21082-alt <123124690+Jerry21082-alt@users.noreply.github.com>
2024-02-13 10:36:07 +0100

    made a change
    made a change

--- components/Footer.js
+++ components/Footer.js
@@ -12,41 +12,125 @@ const Footer = () => {
   return (
     <>
       <footer className="relative bg-navBackground bottom-0 py-[60px] px-0 w-full z-0">
-        <div className="flex justify-center items-center gap-9">
-          <FaFacebookF color="#fff" size={25} />
-          <FaXTwitter color="#fff" size={25} />
-          <FaYoutube color="#fff" size={25} />
-          <FaInstagram color="#fff" size={25} />
-          <FaLinkedinIn color="#fff" size={25} />
-        </div>
+        <section className="m-0 block overflow-hidden text-center">
+          <ul className="mb-0">
+            <li className="py-[15px] px-[10px] inline-block">
+              <Link href="/" className="p-0 block w-[37px]">
+                <svg
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="35"
+                  height="35"
+                  viewBox="0 0 24 24"
+                  fill="#fff"
+                >
+                  <title>social-facebook</title>
+                  <path d="M13 10h3v3h-3v7h-3v-7h-3v-3h3v-1.255c0-1.189 0.374-2.691 1.118-3.512 0.744-0.823 1.673-1.233 2.786-1.233h2.096v3h-2.1c-0.498 0-0.9 0.402-0.9 0.899v2.101z"></path>
+                </svg>
+              </Link>
+            </li>
+            <li className="py-[15px] px-[10px] inline-block">
+              <Link href="/" className="p-0 block w-[37px]">
+                <svg
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="25"
+                  height="35"
+                  viewBox="0 0 32 32"
+                  fill="#fff"
+                >
+                  <title>x</title>
+                  <path d="M24.325 3h4.411l-9.636 11.013 11.336 14.987h-8.876l-6.952-9.089-7.955 9.089h-4.413l10.307-11.78-10.875-14.22h9.101l6.284 8.308zM22.777 26.36h2.444l-15.776-20.859h-2.623z"></path>
+                </svg>
+              </Link>
+            </li>
+            <li className="py-[15px] px-[10px] inline-block">
+              <Link href="/" className="p-0 block w-[37px]">
+                <svg
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="25"
+                  height="35"
+                  viewBox="0 0 20 20"
+                  fill="#fff"
+                >
+                  <title>youtube</title>
+                  <path d="M10 2.3c-9.828 0-10 0.874-10 7.7s0.172 7.7 10 7.7 10-0.874 10-7.7-0.172-7.7-10-7.7zM13.205 10.334l-4.49 2.096c-0.393 0.182-0.715-0.022-0.715-0.456v-3.948c0-0.433 0.322-0.638 0.715-0.456l4.49 2.096c0.393 0.184 0.393 0.484 0 0.668z"></path>
+                </svg>
+              </Link>
+            </li>
+            <li className="py-[15px] px-[10px] inline-block">
+              <Link href="/" className="p-0 block w-[37px]">
+                <svg
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="28"
+                  height="35"
+                  viewBox="0 0 24 24"
+                  fill="#fff"
+                >
+                  <title>instagram1</title>
+                  <path d="M7 1c-1.657 0-3.158 0.673-4.243 1.757s-1.757 2.586-1.757 4.243v10c0 1.657 0.673 3.158 1.757 4.243s2.586 1.757 4.243 1.757h10c1.657 0 3.158-0.673 4.243-1.757s1.757-2.586 1.757-4.243v-10c0-1.657-0.673-3.158-1.757-4.243s-2.586-1.757-4.243-1.757zM7 3h10c1.105 0 2.103 0.447 2.828 1.172s1.172 1.723 1.172 2.828v10c0 1.105-0.447 2.103-1.172 2.828s-1.723 1.172-2.828 1.172h-10c-1.105 0-2.103-0.447-2.828-1.172s-1.172-1.723-1.172-2.828v-10c0-1.105 0.447-2.103 1.172-2.828s1.723-1.172 2.828-1.172zM16.989 11.223c-0.15-0.972-0.571-1.857-1.194-2.567-0.754-0.861-1.804-1.465-3.009-1.644-0.464-0.074-0.97-0.077-1.477-0.002-1.366 0.202-2.521 0.941-3.282 1.967s-1.133 2.347-0.93 3.712 0.941 2.521 1.967 3.282 2.347 1.133 3.712 0.93 2.521-0.941 3.282-1.967 1.133-2.347 0.93-3.712zM15.011 11.517c0.122 0.82-0.1 1.609-0.558 2.227s-1.15 1.059-1.969 1.18-1.609-0.1-2.227-0.558-1.059-1.15-1.18-1.969 0.1-1.609 0.558-2.227 1.15-1.059 1.969-1.18c0.313-0.046 0.615-0.042 0.87-0.002 0.74 0.11 1.366 0.47 1.818 0.986 0.375 0.428 0.63 0.963 0.72 1.543zM17.5 7.5c0.552 0 1-0.448 1-1s-0.448-1-1-1-1 0.448-1 1 0.448 1 1 1z"></path>
+                </svg>
+              </Link>
+            </li>
+            <li className="py-[15px] px-[10px] inline-block">
+              <Link href="/" className="p-0 block w-[37px]">
+                <svg
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="32"
+                  height="35"
+                  viewBox="0 0 32 32"
+                  fill="#fff"
+                >
+                  <title>linkedin2</title>
+                  <path d="M12 12h5.535v2.837h0.079c0.77-1.381 2.655-2.837 5.464-2.837 5.842 0 6.922 3.637 6.922 8.367v9.633h-5.769v-8.54c0-2.037-0.042-4.657-3.001-4.657-3.005 0-3.463 2.218-3.463 4.509v8.688h-5.767v-18z"></path>
+                  <path d="M2 12h6v18h-6v-18z"></path>
+                  <path d="M8 7c0 1.657-1.343 3-3 3s-3-1.343-3-3c0-1.657 1.343-3 3-3s3 1.343 3 3z"></path>
+                </svg>
+              </Link>
+            </li>
+          </ul>
+        </section>
 
-        <div className="font-[.84em] footer-p text-center uppercase w-full">
-          <div className="px-3 mt-6">
-            <ul className="flex justify-center items-start gap-3 flex-wrap text-snow">
-              <li>
-                <Link href={`/`}>Home</Link>
-              </li>
-              <li>
-                <Link href={`/`}>Contact Us</Link>
-              </li>
-              <li>
-                <Link href={`/`}>Privacy</Link>
-              </li>
-              <li>
-                <Link href={`/`}>Terms of use</Link>
-              </li>
-              <li>
-                <Link href={`/`}>Accessibility</Link>
-              </li>
-              <li>
-                <Link href={`/`}>Foia</Link>
-              </li>
-              <li>
-                <Link href={`/`}>No fear act</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
+        <section className="pt-[25px] text-center uppercase">
+          <ul className="list-none footer-list">
+            <li className="my-0 mx-[10px] inline-block">
+              <Link
+                href="/"
+                className="inline-block relative text-snow leading-[2.5]"
+              >
+                Home
+              </Link>
+            </li>
+            <li className="my-0 mx-[10px] inline-block">
+              <Link href="/" className="inline-block relative text-snow leading-[2.5]">
+                Contact us
+              </Link>
+            </li>
+            <li className="my-0 mx-[10px] inline-block">
+              <Link href="/" className="inline-block relative text-snow leading-[2.5]">
+                Terms of use
+              </Link>
+            </li>
+            <li className="my-0 mx-[10px] inline-block">
+              <Link href="/" className="inline-block relative text-snow leading-[2.5]">
+                Accessibility
+              </Link>
+            </li>
+            <li className="my-0 mx-[10px] inline-block">
+              <Link href="/" className="inline-block relative text-snow leading-[2.5]">
+                Foia
+              </Link>
+            </li>
+            <li className="my-0 mx-[10px] inline-block">
+              <Link href="/" className="inline-block relative text-snow leading-[2.5]">
+                No fear act
+              </Link>
+            </li>
+          </ul>
+        </section>
       </footer>
     </>
   );
